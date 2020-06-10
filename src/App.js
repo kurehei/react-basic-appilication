b3baa730c6655b9d9f435f13c937d660d9527a18
榑林晃平 <kureykou321@gmail.com>
2020-06-11 07:22:28 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 
 
-class App extends Component {
-  render() {
+//class App extends Component {
+  // クラスコンポーネント
+  //render() {
     // JSX内に変数も展開できる
-    return(
-      <React.Fragment>
-        <button type="button" onClick={()=>{window.alert("Click!!")}}></button>
-        <p>aa</p>
-      </React.Fragment>
-    )
-  }
+    //return(
+      //<React.Fragment>
+        //<button type="button" onClick={()=>{window.alert("Click!!")}}></button>
+        //<p>aa</p>
+      //</React.Fragment>
+    //)
+  //}
+//}
+
+const App = function() {
+  return <div>
+          <Cat />
+          <Cat />
+          <Cat />
+        </div>
+}
+
+const Cat = () => {
+  return <div>
+    Meow!
+  </div>
 }
 
 export default App;
